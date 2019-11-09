5fff93125750af9065fa164e66b0b8979798e739
Mason Osborn <osbornm@wit.edu>
2019-11-09 16:06:37 -0500

    Initial code for bot

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, SyncSQLiteProvider } = require('discord.js-commando');
+const path = require('path');
+
+const client = new Client({
+  owner: '171043851178475520',
+});
+
+const db = require('better-sqlite3')('settings.db');
+client.setProvider(new SyncSQLiteProvider(db));
+
+client.registry
+  .registerGroups([
+    ['groups', 'Hackathon groups'],
+    ['info', 'Event information'],
+    ['admin', 'Event administrator commands'],
+  ])
+  .registerDefaults()
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.login(process.env.DISCORD_TOKEN);
